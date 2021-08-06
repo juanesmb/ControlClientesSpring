d5187154e8502dad9f58477f889b86a0c1dc1032
Juan Esteban Moncada <juanestebanmb@ufps.edu.co>
2021-08-06 16:52:54 -0500

    in this commit I added more security functionalities. The main characteristic is the restriction of some functions of the web app depending on the rol of the user logged. with this in mind I have to create two different views to handle the login of each user and the restriccions of some paths.

--- src/main/java/com/example/HolaSpring/web/ControladorInicio.java
+++ src/main/java/com/example/HolaSpring/web/ControladorInicio.java
@@ -6,6 +6,8 @@ import com.example.HolaSpring.service.PersonaService;
 import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -20,10 +22,11 @@ public class ControladorInicio {
     private PersonaService personaService;
     
     @GetMapping("/")
-    public String inicio(Model model)
+    public String inicio(Model model, @AuthenticationPrincipal User user)
     {
         var personas = personaService.listarPersonas();
         log.info("ejecutando el controlador Spring MVC");
+        log.info("usuario que hizo login:" + user);
         model.addAttribute("personas", personas);
         return "index";
     }
--- src/main/java/com/example/HolaSpring/web/SecurityConfig.java
+++ src/main/java/com/example/HolaSpring/web/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.HolaSpring.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,7 +14,7 @@ Clase para la configuración de la seguridad de la aplicación
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
     
     /*
-        Método para agregar y personalizar los usuarios a utilizar para hacer login. esto desactiva el usuario por default
+        AUTENTICACIÓN - Método para agregar y personalizar los usuarios a utilizar para hacer login. esto desactiva el usuario por default
     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception
@@ -31,7 +32,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     }
     
     /*
-        método para restringir las URLs de la aplicación web según el rol del usuario
+        AUTORIZACIÓN - método para restringir las URLs de la aplicación web según el rol del usuario
     */
-    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests() 
+                .antMatchers("/editar/**", "/agregar/**", "/eliminar") //indicamos los paths restringidos
+                    .hasRole("ADMIN") //indicamos los usuarios que tienen acceso a los paths restringidos
+                .antMatchers("/")
+                    .hasAnyRole("USER","ADMIN")
+                .and()
+                    .formLogin() //se configura el form login de la página
+                    .loginPage("/login") //se especifica la vista a usar como login
+                .and()
+                    .exceptionHandling().accessDeniedPage("/errores/403") //se configura la vista a mostrar cuando existe un acceso denegado o restringido
+                ;       
+    }
 }
